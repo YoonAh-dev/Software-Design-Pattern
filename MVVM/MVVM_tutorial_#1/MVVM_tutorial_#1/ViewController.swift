8025f1123dc54a4acef2048d9043738e200737d1
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-04-29 18:23:29 +0900

    🍎 초기 설정

---
+++ MVVM/MVVM_tutorial_#1/MVVM_tutorial_#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_tutorial_#1
+//
+//  Created by SHIN YOON AH on 2021/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
