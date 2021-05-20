84c0fd1e6a6781f342eacd6fe99b282ad1c5622e
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-20 14:50:38 +0900

    [FEAT] DetailViewModel 구현

--- MVVM/MVVM_tutorial_#2/MVVM_tutorial_#2/ViewController/MemoDetailViewController.swift
+++ MVVM/MVVM_tutorial_#2/MVVM_tutorial_#2/ViewController/MemoDetailViewController.swift
@@ -18,11 +18,30 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.contents
+            .bind(to: listTableView.rx.items) { tableview, row, value in
+                switch row {
+                // 첫번째 cell이라면 content cell를 꺼내서 label에 문자열을 표시하고 return
+                case 0:
+                    let cell = tableview.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                // 두번째 cell이라면 dateCell를 꺼내서 같은 방식으로 구현
+                case 1:
+                    let cell = tableview.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
     }
 }
--- MVVM/MVVM_tutorial_#2/MVVM_tutorial_#2/ViewModel/MemoDetailViewModel.swift
+++ MVVM/MVVM_tutorial_#2/MVVM_tutorial_#2/ViewModel/MemoDetailViewModel.swift
@@ -5,8 +5,41 @@
 //  Created by SHIN YOON AH on 2021/05/06.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
     
+    // 이전 scene에서 전달된 메모가 저장
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    // 왜 behaviorsubject인가
+    // 메모를 편집한 다음에 다시 보기화면으로 오면 편집한 화면이 반영되어야 한다
+    // 이렇게 하기 위해서 새로운 문자열 배열을 방출해야 한다
+    // 일반 observable로 하면 이게 불가능하기 때문에 이렇게 선언
+    var contents: BehaviorSubject<[String]>
+    
+    init(memo: Memo,
+         title: String,
+         sceneCoordinator: SceneCoordinatorType,
+         storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title,
+                   sceneCoordinator: sceneCoordinator,
+                   storage: storage)
+    }
 }
