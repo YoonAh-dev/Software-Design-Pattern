7e0ad8cf926e72fe49ea683cafefa7428bc5ae37
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-20 14:30:01 +0900

    [MOD] detail View 구성

--- MVVM/MVVM_tutorial_#2/MVVM_tutorial_#2/ViewController/MemoDetailViewController.swift
+++ MVVM/MVVM_tutorial_#2/MVVM_tutorial_#2/ViewController/MemoDetailViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
     var viewModel: MemoDetailViewModel!
-
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
