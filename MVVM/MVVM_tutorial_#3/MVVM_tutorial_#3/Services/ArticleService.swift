73083dc5666232b3788f0756f1f4ec2421d03cf7
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-21 13:19:54 +0900

    [ADD] Model, Service 구현

---
+++ MVVM/MVVM_tutorial_#3/MVVM_tutorial_#3/Services/ArticleService.swift
@@ -0,0 +1,47 @@
+//
+//  ArticleService.swift
+//  MVVM_tutorial_#3
+//
+//  Created by SHIN YOON AH on 2021/05/21.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+class ArticleService {
+
+    // call back 함수 지옥을 방지한다.
+    func fetchNews() -> Observable<[Article]> {
+        return Observable.create { (observer) -> Disposable in
+            self.fetchNews { (error, articles) in
+                if let error = error {
+                    observer.onError(error)
+                }
+                
+                if let articles = articles {
+                    observer.onNext(articles)
+                }
+                
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private func fetchNews(completion: @escaping((Error?, [Article]?) -> Void)) {
+        let urlString = "https://newsapi.org/v2/everything?q=tesla&from=2021-04-21&sortBy=publishedAt&apiKey=96497db299454870a3b6a9c6bb28236f"
+        
+        guard let url = URL(string: urlString) else { return completion(NSError(domain: "YoonAh0525", code: 404, userInfo: nil), nil)}
+        
+        AF.request(url, method: HTTPMethod.get, parameters: nil, encoding: JSONEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).responseDecodable(of: ArticleResponse.self) { response in
+                if let error = response.error {
+                    return completion(error, nil)
+                }
+                
+                if let articles = response.value?.articles {
+                    return completion(nil, articles)
+                }
+        }
+    }
+}
