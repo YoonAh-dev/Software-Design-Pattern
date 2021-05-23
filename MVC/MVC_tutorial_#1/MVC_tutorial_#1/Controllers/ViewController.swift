8e9f16ded18dc2bc1b8ba460ba8df6f9f8580215
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-24 08:37:57 +0900

    [FEAT] Controller 구현

--- MVC/MVC_tutorial_#1/MVC_tutorial_#1/Controllers/ViewController.swift
+++ MVC/MVC_tutorial_#1/MVC_tutorial_#1/Controllers/ViewController.swift
@@ -13,11 +13,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+        updateUI()
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let choice1 = storyBrain.getChoiceButtonText()
+        let choice2 = storyBrain.getChoice2ButtonText()
+        
+        if userAnswer == choice1 {
+            storyBrain.changeStoryNumber(targetNumber: storyBrain.getChoice1Destination())
+        } else if userAnswer == choice2 {
+            storyBrain.changeStoryNumber(targetNumber: storyBrain.getChoice2Destination())
+        }
+        
+        updateUI()
+    }
+    
+    func configureUI() {
+        choice1Button.backgroundColor = .systemPink
+        choice2Button.backgroundColor = .systemPurple
+        choice1Button.layer.cornerRadius = 15
+        choice2Button.layer.cornerRadius = 15
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "background") ?? .checkmark)
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryLabelText()
+        choice1Button.setTitle(storyBrain.getChoiceButtonText(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2ButtonText(), for: .normal)
+    }
 }
 
