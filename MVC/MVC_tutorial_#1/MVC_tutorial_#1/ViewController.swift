077465ff37a9688296087cc1064e0a0550ae3702
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-23 21:49:55 +0900

    [ADD] MVC Tutorial

---
+++ MVC/MVC_tutorial_#1/MVC_tutorial_#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_tutorial_#1
+//
+//  Created by SHIN YOON AH on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
