1110c6895adf4b8dea39a1d6c0a1df837183c1c5
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-24 08:14:08 +0900

    [FEAT] Storyboard Layout 구성

--- MVC/MVC_tutorial_#1/MVC_tutorial_#1/Controllers/ViewController.swift
+++ MVC/MVC_tutorial_#1/MVC_tutorial_#1/Controllers/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
