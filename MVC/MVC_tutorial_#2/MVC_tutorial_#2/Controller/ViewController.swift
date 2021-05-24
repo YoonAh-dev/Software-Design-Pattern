e3846c837365f0125d04578793690a135e591366
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-24 11:37:05 +0900

    [FEAT] Card게임 로직 구현

--- MVC/MVC_tutorial_#2/MVC_tutorial_#2/Controller/ViewController.swift
+++ MVC/MVC_tutorial_#2/MVC_tutorial_#2/Controller/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    
     var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
-    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    var emojiChoices = ["🎃", "👻"]
+    var emoji: [Int:String] = [:]
 
     @IBOutlet weak var flipCountLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
@@ -25,21 +28,36 @@ class ViewController: UIViewController {
 
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        if let cardNumber = cardButtons.index(of: sender) {
-            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
         } else {
             print("chosen card was not in cardButtons")
         }
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle("", for: .normal)
-            button.backgroundColor = .systemOrange
-        } else {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = .white
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = .white
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? .clear : .systemOrange
+            }
         }
+    }
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        
+        return emoji[card.identifier] ?? "?"
     }
 }
 
--- MVC/MVC_tutorial_#2/MVC_tutorial_#2/Model/Card.swift
+++ MVC/MVC_tutorial_#2/MVC_tutorial_#2/Model/Card.swift
@@ -11,4 +11,15 @@ struct Card {
     var isFaceUp = false
     var isMatched = false
     var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
 }
