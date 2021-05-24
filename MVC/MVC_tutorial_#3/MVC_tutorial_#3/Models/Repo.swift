4f6cf7bfa88dd2a74101101ff694466491b14d7d
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-24 12:36:01 +0900

    [ADD] tutorial3 생성

---
+++ MVC/MVC_tutorial_#3/MVC_tutorial_#3/Models/Repo.swift
@@ -0,0 +1,13 @@
+//
+//  Repo.swift
+//  MVC_tutorial_#3
+//
+//  Created by SHIN YOON AH on 2021/05/24.
+//
+
+import Foundation
+
+struct Repo: Decodable {
+    let id: Int
+    let name: String
+}
---
+++ MVC/MVC_tutorial_#3/MVC_tutorial_#3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_tutorial_#3
+//
+//  Created by SHIN YOON AH on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
