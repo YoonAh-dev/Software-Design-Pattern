9fed5b234c74223b61627998d2bdee61c30a8646
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-12-01 19:16:47 +0900

    [ADD] ochococo Example

---
+++ Design-Pattern/Adapter/Adapter-Example/Adapter-Example/NewDeathStarSuperLaserAiming.swift
@@ -0,0 +1,13 @@
+//
+//  NewDeathStarSuperLaserAiming.swift
+//  Adapter-Example
+//
+//  Created by SHIN YOON AH on 2021/12/01.
+//
+
+import Foundation
+
+protocol NewDeathStarSuperLaserAiming {
+    var angleV: Double { get }
+    var angleH: Double { get }
+}
--- Design-Pattern/Adapter/Adapter-Example/Adapter-Example/ViewController.swift
+++ Design-Pattern/Adapter/Adapter-Example/Adapter-Example/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     let adaptee = Adaptee()
+    let target = OldDeathStarSuperLaserTarget(angleHorizontal: 14.0, angleVertical: 12.0)
+    lazy var newFormat = NewDeathStarSuperLaserTarget(target)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,10 @@ class ViewController: UIViewController {
         
         /// Call from adapter
         Client.specificRequest(target: Adapter(adaptee))
+        
+        /// newformat
+        print("angleH", newFormat.angleH)
+        print("angleV", newFormat.angleV)
     }
 
 
